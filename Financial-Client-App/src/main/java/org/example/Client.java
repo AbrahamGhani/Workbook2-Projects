e71d471c956393bfb8ff91b63ae298351611168a
Abraham Ghani <AbrahamKosmoGhani@gmail.com>
2025-04-16 10:12:22 -0700

    finished txt file and added client.java
    finished txt file planning and added vars for client file

---
+++ Financial-Client-App/src/main/java/org/example/Client.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Client {
+
+    // storing information - first name, last name
+    // business name
+    //email address
+    //phone number
+
+    private String firstName;
+    private String lastName;
+    private String businessName;
+    private String emailAddress;
+    private String phoneNumber;
+
+
+
+
+
+}
--- Financial-Client-App/src/main/java/org/example/Main.java
+++ Financial-Client-App/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 /// Financial client app exercise
 
 
-        
+
 
 
 
